e7040c089e709152b851f5605dec038853c00c47
Ernesto <epachecomorelos@gmail.com>
2020-01-10 16:27:29 +0000

    Adding axios, and service call

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const {argvWeather : argv} = require('./config/yargs');
+const axios = require('axios');
 
-console.log(argv)
+axios({
+    "method":"GET",
+    "url":"https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php",
+    "headers":{
+      "x-rapidapi-key":"8f50cb10bdmshec653a3c7ce5202p16a059jsna1000593a272"
+    },
+    "params":{
+      "location":argv.address
+    }
+    })
+    .then((response)=>{
+      console.log(response.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
